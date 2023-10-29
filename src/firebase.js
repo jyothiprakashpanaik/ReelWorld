27237410e807ef93439df5f05bbaeba74f79dfb2
Janvit __ jp <jyothiprakashpatnaik@gmail.com>
2023-10-29 23:03:50 +0530

    src/Components/Profile.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,25 @@ import "firebase/compat/auth";
 import "firebase/compat/storage";
 import "firebase/compat/firestore";
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDMXo8muzCzFc5vnmFVKHHU0HrqpMe1SHY",
+//     authDomain: "reels-61508.firebaseapp.com",
+//     projectId: "reels-61508",
+//     storageBucket: "reels-61508.appspot.com",
+//     messagingSenderId: "1024782691847",
+//     appId: "1:1024782691847:web:b38a95cde0897dc9c104a2",
+//     measurementId: "G-LPSS07KSBF"
+//   };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDMXo8muzCzFc5vnmFVKHHU0HrqpMe1SHY",
-    authDomain: "reels-61508.firebaseapp.com",
-    projectId: "reels-61508",
-    storageBucket: "reels-61508.appspot.com",
-    messagingSenderId: "1024782691847",
-    appId: "1:1024782691847:web:b38a95cde0897dc9c104a2",
-    measurementId: "G-LPSS07KSBF"
-  };
+  apiKey: "AIzaSyCD-eU3nN_D7V8r0Mlljy_nyXUsgl9eJD8",
+  authDomain: "reel-world.firebaseapp.com",
+  projectId: "reel-world",
+  storageBucket: "reel-world.appspot.com",
+  messagingSenderId: "589543010802",
+  appId: "1:589543010802:web:7cb56a0ea341b7fdfc3b6b",
+  measurementId: "G-GV2MSZD65P"
+};
 
 firebase.initializeApp(firebaseConfig);
 
