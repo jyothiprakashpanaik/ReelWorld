91df808ec31ea24d9c4d81200c6c275ba5e111aa
Janvit __ jp <jyothiprakashpatnaik@gmail.com>
2023-10-29 23:02:58 +0530

    Added video progress bar

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,81 +1,131 @@
 import React, { useRef, useEffect, useState, useImperativeHandle } from 'react'
 import "./Video.css";
 import ReactDOM from 'react-dom';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+
+import LinearProgress from '@mui/material/LinearProgress';
 
 
+const Video = React.forwardRef((props, ref) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isMuted, setIsMuted] = useState(true);
+  const [showMuteIcon, setShowMuteIcon] = useState(false);
+  const [progress, setProgress] = React.useState(0);
 
-const Video = React.forwardRef((props,ref) => {
-
-  useImperativeHandle(ref, ()=>{
-    return {pause: handleVideo, currentTime: getCurrentTime}
+  useImperativeHandle(ref, () => {
+    return { pause: handleVideo, currentTime: getCurrentTime }
   });
 
   const videoRef = useRef(null);
 
-  function handleVideo() {
-    console.log("Before Is Paused:",videoRef.current.paused, props.currentId);
-    if(videoRef.current.paused===false){
-      videoRef.current.pause();
+  function handleVideo(cmtSec) {
+    console.log("Before Is Paused:", videoRef.current.paused, props.currentId);
+    try {
+      if (cmtSec) {
+        if (videoRef.current.paused === false) {
+          videoRef.current.pause();
+        }
+        else {
+          videoRef.current.play();
+        }
+      }
     }
-    else{
-      videoRef.current.play();
+    catch (err) {
+      console.log(err);
     }
-    console.log("After Is Paused:",videoRef.current.paused);
+
+    console.log("After Is Paused:", videoRef.current.paused);
   }
 
-  function getCurrentTime(){
+  function getCurrentTime() {
     return videoRef.current.currentTime;
   }
 
   const handleClick = (e) => {
     e.preventDefault();
     e.target.muted = !e.target.muted;
+    setShowMuteIcon(true);
+    setIsMuted(e.target.muted);
+
+
+    setTimeout(() => {
+      setShowMuteIcon(false);
+    }, 5000);
+
   }
 
   const handleScroll = (e) => {
     let next = ReactDOM.findDOMNode(e.target).parentElement.nextSibling;
-
-    if (next) {
-      next.scrollIntoView({ behavior: 'smooth' })
+    try {
+      if (next) {
+        next.scrollIntoView({ behavior: 'smooth' })
+      }
+    }
+    catch (err) {
+      console.log(err);
     }
   }
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+  };
 
-  // useEffect(() => {
-  //   const options = {
-  //     root: null,
-  //     rootMargin: '0px',
-  //     threshold: 0.5,
-  //   };
+  const handleIntersection = (entries) => {
+    entries.forEach((entry) => {
+      try {
+        if (entry.isIntersecting && isLoaded) {
+          videoRef.current.play();
+        } else {
+          videoRef.current.pause();
+        }
+      }
+      catch (err) {
+        console.log(err);
+      }
+    });
+  };
 
-  //   const handleIntersection = (entries) => {
-  //     entries.forEach((entry) => {
-  //       if (entry.isIntersecting) {
-  //         videoRef.current.play();
-  //       } else {
-  //         videoRef.current.pause();
-  //       }
-  //     });
-  //   };
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection, options);
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
 
-  //   const observer = new IntersectionObserver(handleIntersection, options);
+    }
 
-  //   if (videoRef.current) {
-  //     observer.observe(videoRef.current);
-  //   }
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, [isLoaded]);
 
-  //   return () => {
-  //     if (videoRef.current) {
-  //       observer.unobserve(videoRef.current);
-  //     }
-  //   };
-  // }, []);
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.addEventListener('timeupdate', () => {
+        if (videoRef.current) {
+          let currentTime = videoRef.current.currentTime;
+          let duration = videoRef.current.duration;
+          setProgress((currentTime / duration) * 100);
+        }
+      });
+    }
+  }, []);
 
   console.count(`Re Rendered ${props.currentId}`);
 
   return (
-    <video ref={videoRef} className='videoContent' muted={true} onClick={handleClick} autoPlay={false} onEnded={handleScroll} controls>
-      <source src={props.postUrl} type="video/mp4"/>
-    </video>
+    <div style={{display:'flex', flexDirection:"column", alignItems:"center"}}>
+      <video onLoadedMetadata={() => setIsLoaded(true)} ref={videoRef} className='videoContent' muted={true} onClick={handleClick} autoPlay={false} onEnded={handleScroll} controls={false} loop={false} src={props.postUrl} type="video/mp4" />
+      <div >
+        {showMuteIcon && (isMuted ? <VolumeOffIcon onClick={handleClick} 
+        style={{ color: "white", background: "rgba(0, 0, 0, 0.5)", borderRadius: "50%", padding: "1%", top: "2.5%", right: "5%", position: "absolute", cursor: "pointer" }}/> : <VolumeUpIcon onClick={handleClick} 
+        style={{ color: "white", background: "rgba(0, 0, 0, 0.5)", borderRadius: "50%", padding: "1%", top: "2.5%", right: "5%", position: "absolute", cursor: "pointer" }}/>)}
+      </div>
+      <LinearProgress variant="determinate" value={progress} color="error" 
+      style={{ bottom: "8px", width: "98%", borderRadius:"5px" }}/>
+    </div>
   )
 })
 
