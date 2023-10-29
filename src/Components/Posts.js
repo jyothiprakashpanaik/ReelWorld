b2634425f12542195426d07f179554e9de694aec
Janvit __ jp <jyothiprakashpatnaik@gmail.com>
2023-10-29 23:05:56 +0530

    Updated Post removed useEffect

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -3,19 +3,18 @@ import { database } from '../firebase';
 import CircularProgress from '@mui/material/CircularProgress';
 import Avatar from '@mui/material/Avatar';
 import CommentIcon from '@mui/icons-material/Comment';
-import DeleteIcon from '@mui/icons-material/Delete';
 import Dialog from '@mui/material/Dialog';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import Like from './Like';
 import Video from './Video';
 import AddComment from './AddComment';
 import Like2 from './Like2';
 import Comments from './Comments';
 import "./Posts.css";
+import { useNavigate } from 'react-router-dom';
 
 
 
@@ -24,33 +23,21 @@ function Posts({ userData }) {
     const [open, setOpen] = useState(null);
 
     const videoRefs = useRef({});
+    const navigate = useNavigate();
 
     console.log(videoRefs);
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
 
-    let options = { threshold: 0.6 };
-    let callback = (entries) => {
-        entries.forEach((entry) => {
-            // console.log(entry);
-            let ele = entry.target.childNodes[0];
-            ele.play().then(() => {
-                if (!ele.paused && !entry.isIntersecting) {
-                    ele.pause();
-                }
-            })
-        })
-    }
-    let observer = new IntersectionObserver(callback, options);
-
     const handleClickOpen = (id) => {
-        videoRefs.current[id].pause();
+        console.log("VVVV",videoRefs.current[id]);
+        videoRefs.current[id].pause(true);
         setOpen(id);
     };
 
     const handleClose = (id) => {
-        videoRefs.current[id].pause();
+        videoRefs.current[id].pause(true);
         setOpen(null);
     };
 
@@ -77,78 +64,65 @@ function Posts({ userData }) {
                 data.push({ ...doc.data(), postId: doc.id });
             });
             setPosts(data);
-            console.log(data);
+            console.log("Updated Data", data);
         })
         return () => { unsub(); }
     }, []);
 
-
-    useEffect(() => {
-        let elements = document.querySelectorAll(".videos");
-
-        elements.forEach((element) => {
-            observer.observe(element);
-        });
-
-        return () => {
-            elements.forEach((element) => {
-                observer.unobserve(element);
-            });
-
-            observer.disconnect();
-        }
-    }, [posts]);
+    console.count("Re render");
+    console.log(posts);
+    console.log(userData);
 
     return (
         <div>{
             (!posts || !userData) ? <CircularProgress color="secondary" /> : <div className='videoContainer'>
                 {posts.map((post, index) => (
-                    <React.Fragment key={index}>
-                        <div className="videos">
-                            <Video postUrl={post.postUrl} ref={(ref) => (videoRefs.current[index] = ref)} currentId={index} />
-                            <div className='collection' style={{ display: "flex" }}>
-                                <div className='faAvatar' style={{ display: "flex", justifyContent: "flex-end" }} >
-                                    <Avatar className='avatar' src={post.userProfile} />
-                                    <h4>{post.userName}</h4>
-                                </div>
-                                <div className='likeAndComment'>
-                                    <Like postData={post} userDetails={userData} />
-                                    <CommentIcon className='commentIcon' onClick={() => handleClickOpen(index)} />
-                                    {/* {userData.userId === post.userId && <DeleteIcon style={{ color: "white" }} onClick={() => handleDelete(post.postId)} />} */}
-                                </div>
-                                {open === index ? <Dialog
-                                    fullScreen={fullScreen}
-                                    open={!!(open + 1)}
-                                    onClose={() => { handleClose(index) }}
-                                    aria-labelledby="responsive-dialog-title"
-                                    fullWidth={true}
-                                    maxWidth='md'
-                                >
-                                    <div className='modalContainer'>
-                                        <div className='videoContainer1'>
-                                            <div className="modalVedio">
-                                                <video src={post.postUrl} autoPlay={true} controls />
-                                            </div>
-                                        </div>
-                                        <div className='commentContainer'>
-                                            <Card className='card1' style={{ height: "70vh", overflowY: "scroll", scrollSnapType: "y mandatory", scrollbarWidth: "none" }}>
-                                                <Comments postData={post} />
-                                            </Card>
-                                            <Card variant="outlined" className='card2'>
-                                                <Typography style={{ textAlign: "center" }}>{`Liked by ${post.likes.length} users`}</Typography>
-                                                <div className="likeComment" style={{ display: "flex", padding: "0.5rem 0", alignItems: "center" }}>
-                                                    <Like2 postData={post} userDetails={userData} style={{ display: "flex", alignItems: "center", justifyContent: "center" }} />
-                                                    <AddComment postData={post} userDetails={userData} />
-                                                </div>
-                                            </Card>
-
+                    <div className="videos" key={index}>
+                        <Video postUrl={post.postUrl} ref={(ref) => (videoRefs.current[index] = ref)} currentId={index} />
+                        <div className='collection' style={{ display: "flex" }}>
+                            <div className='faAvatar' onClick={()=>{navigate(`/profile/${userData.userId}`)}} style={{ display: "flex", justifyContent: "flex-end", width: "100%" }} >
+                                <Avatar className='avatar' src={post.userProfile} />
+                                <h4>{post.userName}</h4>
+                            </div>
+                            <div className='likeAndComment'>
+                                <Like postData={post} userDetails={userData} />
+                                <CommentIcon className='commentIcon' onClick={() => handleClickOpen(index)} />
+                                {/* {userData.userId === post.userId && <DeleteIcon style={{ color: "white" }} onClick={() => handleDelete(post.postId)} />} */}
+                            </div>
+                            {open === index ? <Dialog
+                                fullScreen={fullScreen}
+                                open={!!(open + 1)}
+                                onClose={() => { handleClose(index) }}
+                                aria-labelledby="responsive-dialog-title"
+                                fullWidth={true}
+                                maxWidth='md'
+                            >
+                                <div className='modalContainer'>
+                                    <div className='videoContainer1'>
+                                        <div className="modalVedio">
+                                            {/* <video src={post.postUrl} autoPlay={false} controls /> */}
+                                            <Video postUrl={post.postUrl} currentId={index}/>
                                         </div>
                                     </div>
-                                </Dialog>
-                                    : <></>}
-                            </div>
+                                    <div className='commentContainer'>
+                                        <Card className='card1' style={{ height: "70vh", overflowY: "scroll", scrollSnapType: "y mandatory", scrollbarWidth: "none" }}>
+                                            <Comments postData={post} />
+                                        </Card>
+                                        <Card variant="outlined" className='card2'>
+                                            <Typography style={{ textAlign: "center" }}>{`Liked by ${post.likes.length} users`}</Typography>
+                                            <div className="likeComment" style={{ display: "flex", padding: "0.5rem 0", alignItems: "center" }}>
+                                                <Like2 postData={post} userDetails={userData} style={{ display: "flex", alignItems: "center", justifyContent: "center" }} />
+                                                <AddComment postData={post} userDetails={userData} />
+                                            </div>
+                                        </Card>
+
+                                    </div>
+                                </div>
+                            </Dialog>
+                                : <></>}
                         </div>
-                    </React.Fragment>
+                    </div>
+
                 ))}
             </div>
         }
