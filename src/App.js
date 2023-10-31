8762846e3147fe9e71ea1164e453fa1d57620f9d
Janvit __ jp <jyothiprakashpatnaik@gmail.com>
2023-11-01 02:06:49 +0530

    Moved BrowserRouter from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import SignUp from './Components/Signup';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LogIn from './Components/Login';
 import ForgetPassword from './Components/ForgetPassword';
 import { AuthProvider } from './Context/AuthContext';
@@ -8,12 +8,13 @@ import Feed from './Components/Feed';
 import PrivateRoute from './Components/PrivateRoute';
 import Profile from './Components/Profile';
 import ManageAccount from './Components/ManageAccount';
+import SideNavBar from './Components/SideNavBar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
+
+    <AuthProvider>
+      <Routes>
           <Route path="/login" element={<LogIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forgetpassword" element={<ForgetPassword />} />
@@ -27,9 +28,8 @@ function App() {
           <Route exact path="/" element={<PrivateRoute />}>
             <Route exact path="/" element={<Feed />} />
           </Route>
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+      </Routes>
+    </AuthProvider>
   );
 }
 
