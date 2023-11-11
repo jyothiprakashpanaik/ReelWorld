b2f833f83379caf7bbd28b35cbefce17b4c6a45c
Janvit __ jp <jyothiprakashpatnaik@gmail.com>
2023-11-12 01:37:37 +0530

    added styles

---
+++ src/Styles/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles({
+    text1: {
+        color: "grey",
+        textAlign: "center"
+    },
+    card2: {
+        marginTop: "2%",
+    }
+});
